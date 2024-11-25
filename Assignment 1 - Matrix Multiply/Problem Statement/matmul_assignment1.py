3a6f4823f868f1e4504eb969d3d40a17c6c43516
lazarsalt14 <149092875+lazarsalt14@users.noreply.github.com>
2024-11-25 19:02:21 +0530

    Add files via upload

---
+++ Assignment 1 - Matrix Multiply/Problem Statement/matmul_assignment1.py
@@ -0,0 +1,7 @@
+def matrix_multiply(matrix1, matrix2):
+    # This function should be implemented by the students
+    # Placeholder implementation that always raises NotImplementedError
+    raise NotImplementedError("Matrix multiplication function not implemented")
+
+
+
